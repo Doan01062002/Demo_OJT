5fe72da2a6f997d4ab4e5a23bb3acdfd3fd6d25f
Doan01062002 <vandoan01062002@gmail.com>
2025-08-11 15:31:13 +0700

    update tien

--- tiendepzai.java
+++ tiendepzai.java
@@ -2,6 +2,8 @@ public class tiendepzai {
     //tien dep zai
     public static void main(String[] args) {
         System.out.println("tien ngu nhu c.ho");
+        System.out.println("tien ngu");
+        System.out.println("tien vcl");
     }
 
     // Tien hoi ga
