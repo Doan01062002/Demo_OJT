eac41532c98188e63228809514271de36749157d
Doan01062002 <vandoan01062002@gmail.com>
2025-08-11 15:47:04 +0700

    test1

--- tiendepzai.java
+++ tiendepzai.java
@@ -4,6 +4,10 @@ public class tiendepzai {
         System.out.println("tien ngu nhu c.ho");
         System.out.println("tien ngu");
         System.out.println("tien vcl");
+
+        int a=1;
+        int b=2;
+        System.out.println(a+a);
     }
 
     // Tien hoi ga
