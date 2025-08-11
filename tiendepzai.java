8c69dbd195569bd8d01c7637b03ef4aa8ef20c85
CanhVeTaiGia <nghia120425@gmail.com>
2025-08-11 15:11:52 +0700

    feature: fix tiendepzai

--- tiendepzai.java
+++ tiendepzai.java
@@ -1,6 +1,6 @@
 public class tiendepzai {
     //tien dep zai
     public static void main(String[] args) {
-        System.out.println("tien dep dai");
+        System.out.println("tien ngu nhu c.ho");
     }
 }
